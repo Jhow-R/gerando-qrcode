f93e5eff97f92a217fcf8dcf1c986387fb96ddb9
Jhonathan Araujo <JhonathanAraujoBR@Hotmail.com>
2020-04-26 16:22:26 -0300

    Gerando QR Code

---
+++ GerandoQRCode/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+
+namespace GerandoQRCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                QRCodeEncoder encoder = new QRCodeEncoder();
+                QRCodeDecoder decoder = new QRCodeDecoder();
+
+                encoder.QRCodeBackgroundColor = System.Drawing.Color.White;
+                encoder.QRCodeForegroundColor = System.Drawing.Color.Black;
+                encoder.CharacterSet = "UTF-8";
+                encoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;
+                encoder.QRCodeScale = 6;
+                encoder.QRCodeVersion = 0;
+                encoder.QRCodeErrorCorrect = QRCodeEncoder.ERROR_CORRECTION.Q;
+
+                Image QRCode = encoder.Encode(txtConteudo.Text);
+                picQrCode.Image = QRCode;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro : " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
